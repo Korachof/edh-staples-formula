606d9694e436c39f62e6664dce67fe0233f38f05
Christopher Partin <christopherpartin11@gmail.com>
2023-11-18 12:34:49 -0800

    Added images to a button_images folder and connected each button to its respective image.
    The images are confirmed to work, but the window that is created cuts off most of the  green image. Tried to replace button.pack() with button.grid(), but so far haven't found a solution. Will read documents and see what I can find.

--- main.py
+++ main.py
@@ -4,19 +4,30 @@ window = Tk()
 
 # "Please select which colors are included in your deck below"
 
-button1 = Button(window)
-button1.pack()
+white_button = PhotoImage(file="button_images/white.png")
+blue_button = PhotoImage(file="button_images/blue.png")
+black_button = PhotoImage(file="button_images/black.png")
+red_button = PhotoImage(file="button_images/red.png")
+green_button = PhotoImage(file="button_images/green.png")
 
-button2 = Button(window)
-button2.pack()
+button1 = Button(window,
+                 image=white_button)
+button1.grid()
 
-button3 = Button(window)
-button3.pack()
+button2 = Button(window,
+                 image=blue_button)
+button2.grid()
 
-button4 = Button(window)
-button4.pack()
+button3 = Button(window,
+                 image=black_button)
+button3.grid()
 
-button5 = Button(window)
-button5.pack()
+button4 = Button(window,
+                 image=red_button)
+button4.grid()
+
+button5 = Button(window,
+                 image=green_button)
+button5.grid()
 
 window.mainloop()

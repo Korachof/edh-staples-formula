06af7a4544e080905ad7d4e7050fa12248579347
Christopher Partin <christopherpartin11@gmail.com>
2023-11-18 14:23:50 -0800

    Changed the buttons to a grid style and used row and column to place them in horizontal view instead. They are all visible now!
    Also added a geometry to the window to expand it. The values are temporary.
    
    Added a simple title.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from tkinter import *
 
 window = Tk()
+window.title("EDH Staple Compiler")
+
+window.geometry('1280x720')
+
+# canvas = Canvas(window, width= 1000, height= 750, bg="#B2C248")
+# canvas.create_text(300, 50, text="HELLO WORLD", fill="black", font=('Helvetica 15 bold'))
+# canvas.pack()
 
 # "Please select which colors are included in your deck below"
 
@@ -12,22 +19,26 @@ green_button = PhotoImage(file="button_images/green.png")
 
 button1 = Button(window,
                  image=white_button)
-button1.grid()
+button1.grid(row=0, column=0)
 
 button2 = Button(window,
                  image=blue_button)
-button2.grid()
+button2.grid(row=0,
+             column=1)
 
 button3 = Button(window,
                  image=black_button)
-button3.grid()
+button3.grid(row=0,
+             column=2)
 
 button4 = Button(window,
                  image=red_button)
-button4.grid()
+button4.grid(row=0,
+             column=3)
 
 button5 = Button(window,
                  image=green_button)
-button5.grid()
+button5.grid(row=0,
+             column=4)
 
 window.mainloop()

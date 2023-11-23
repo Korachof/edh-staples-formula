4c186fe68dfbe09c07fec44fff304cc598ce387e
Christopher Partin <christopherpartin11@gmail.com>
2023-11-22 18:44:40 -0800

    Commented out the first attempt at creating a huge class as it was getting quite confusing trying it that way. May return in the future.
    For now, started creating the StartPage class. Played around with rows and columns for grid to truly understand where items will be placed.
    
    Adding two text labels. In minimal testing, both show correctly and are in the correct place. Window works correctly with correct pixels.

--- ui_windows.py
+++ ui_windows.py
@@ -3,21 +3,74 @@ from tkinter import ttk
 from tkinter import *
 
 
-class ui(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-       
-        self.wm_title("MTG Staple Compiler")
+# class UI(tk.Tk):
+#     def __init__(self, *args, **kwargs):
+#         tk.Tk.__init__(self, *args, **kwargs)
+#         self.ui_frames = {}
+#         self.wm_title("MTG Staple Compiler")
         
-        ui_frame = tk.Frame(
-            self, 
-            width = 1280, 
-            height = 720
-        )
+#         ui_frame = tk.Frame(
+#             self, 
+#             width = 1280, 
+#             height = 720
+#         )
         
-        ui_frame.pack(side = "top", fill = "both", expand = True)
+#         ui_frame.pack(side = "top", fill = "both", expand = True)
+        
+#         # make a list of ui windows to swap between 
+
+#         ui_windows = (StartPage, MakeAccountPage, EditAccountPage, ColorSelectPage, ResultsPage, DownloadPage)
+    
+#         for window in ui_windows: 
+#             frame = window(ui_frame, self)
+            
+
+class StartPage(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
+        self.title("MTG Staple Compiler")
+        self.geometry("1280x720")
+        
+        # Configure the row and 5 columns for the buttons. 
+        Grid.rowconfigure(self,0,weight=1)
+        Grid.rowconfigure(self,1,weight=1)
+        Grid.rowconfigure(self,2,weight=1)
+        Grid.columnconfigure(self,0,weight=1)
+        Grid.columnconfigure(self,1,weight=1)
+        Grid.columnconfigure(self,2,weight=1)
+        Grid.columnconfigure(self,3,weight=1)
+        Grid.columnconfigure(self,4,weight=1)
+        
+        self.welcome_label = ttk.Label(self, text="Welcome to the MTG Staple Compiler")
+        self.choose_label = ttk.Label(self, text="Please choose from the options below")
+        
+        # configure buttons
+        
+
+        self.welcome_label.grid(row=0, column=0, padx=1, pady=1)
+        self.choose_label.grid(row=1, column=0, padx=1, pady=1)
+        
+
+    
+
+class MakeAccountPage:
+    pass
+
+class EditAccountPage:
+    pass
+
+class ColorSelectPage:
+    pass
+
+class ResultsPage:
+    pass
+
+class DownloadPage:
+    pass
+
 
 
 if __name__ == "__main__":
-    testObj = ui()
+    testObj = StartPage()
     testObj.mainloop()
